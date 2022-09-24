a80b6b5e6144e6b21842334407a2704da678cada
MicRedd1985 <micredd1985@gmail.com>
2022-09-24 16:48:54 +0300

    сделано 3 задачи 6 ДЗ

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ingredients.forEach (Element => {
+  let listItem = document.createElement("li");
+  listItem.innerHTML = Element;
+  listItem.classList.add ("item");
+  
+  document.querySelector("#ingredients").append(listItem);
+} )
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,12 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector (".gallery");
+
+images.forEach (img => {
+  gallery.insertAdjacentHTML ("afterbegin", insertImage (img.url, img.alt));
+});
+function insertImage (src, alt) {
+  return `<li><image src='${src}' alt='${alt}'></image></li>`;
+}
